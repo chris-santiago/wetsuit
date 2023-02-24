5f293dd96ed65444e251f2ab4a13ac106bb59f7b
chris-santiago <cjsantiago@gatech.edu>
2023-02-24 11:15:11 -0500

    module imports

--- wetsuit/__init__.py
+++ wetsuit/__init__.py
@@ -1,4 +1,8 @@
 """Package initialization."""
 from importlib.metadata import version
 
+from wetsuit.models import WetsuitClassifier, WetsuitRegressor
+from wetsuit.transforms import H2oFrameTransformer
+
+
 __version__ = version("wetsuit")
